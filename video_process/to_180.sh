e7c8f0daa30bc5614e933ecf53dce0d38441e5b8
Yu-Fan Tung <yftung@visualon.com>
2018-02-27 13:00:25 +0800

    Add some trained models

---
+++ video_process/to_180.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+    echo "Illegal number of parameters"
+    exit 1
+fi
+
+fileList=$(ls -1 $1)
+
+for f in $fileList
+do
+  echo $f
+  convert $1/$f -resize 320x180 $2/$f
+done
+
